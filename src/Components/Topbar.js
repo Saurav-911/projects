6936307bda593c5fce7b7cdd771d0d8158a5653d
unknown <bhoj.basnet@study.lbef.edu.com>
2025-10-31 13:28:28 +0545

    improvement

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Topbar() {
   return (
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"auto",height:"500px",border:"2px solid white",marginBottom:"20px",color:"white",textAlign:"center",backgroundColor:"#110111ff",fontFamily: "Arial, sans-serif"}}> 
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"start",height:"500px",border:"2px solid white",color:"white",backgroundColor:"#110111ff",fontFamily: "Arial, sans-serif"}}> 
       <h1 style={{fontSize:"100px "}} >Portfolio</h1>
+      <p>by Saurav Basnet</p>
     </div>
   )
 }
